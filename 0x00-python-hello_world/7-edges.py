6c6f6644ad53c55ff28ad78c1315746db7fed837
Adetobaa <eadeleke201@gmail.com>
2023-10-03 18:58:44 -0700

    another update

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,9 +4,9 @@ word = "Holberton"
 
 word_first_3 = word[:3]
 
-word_last_2 = word[7:]
+word_last_2 = word[-2:]
 
-middle_word = word[1:8]
+middle_word = word[1:-1]
 
 print("First 3 letters: {}".format(word_first_3))
 
